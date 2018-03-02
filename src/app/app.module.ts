73edb23434bee4433604f45d377b01dddaef70db
cpt <p.sanchez.fr@gmail.com>
2018-03-02 12:02:31 +0100

    added component dataFieldTemplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuTemplateComponent } from './menu-template/menu-template.component';
 import { ButtonTemplateComponent } from './button-template/button-template.component';
+import { DataFieldTemplateComponent } from './data-field-template/data-field-template.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuTemplateComponent,
-    ButtonTemplateComponent
+    ButtonTemplateComponent,
+    DataFieldTemplateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/data-field-template/data-field-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-field-template',
+  templateUrl: './data-field-template.component.html',
+  styleUrls: ['./data-field-template.component.css']
+})
+export class DataFieldTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
