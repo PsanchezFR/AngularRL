93bb6c84fb7ce0e03d67fd0496ae6ae7fe09f0cc
cpt <p.sanchez.fr@gmail.com>
2018-03-02 11:54:36 +0100

    added menuTemplate and buttonTemplate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MenuTemplateComponent } from './menu-template/menu-template.component';
+import { ButtonTemplateComponent } from './button-template/button-template.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTemplateComponent,
+    ButtonTemplateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/button-template/button-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-template',
+  templateUrl: './button-template.component.html',
+  styleUrls: ['./button-template.component.css']
+})
+export class ButtonTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
